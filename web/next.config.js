4b555bf97381cd7da7ae74444d5e8765d3839212
Moreblessing Nyemba <morebnyemba@gmail.com>
2025-12-19 20:36:06 +0200

    Update API base URL in environment file, add console removal in Next.js config, and create .dockerignore file

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   
   images: {
     remotePatterns: [
