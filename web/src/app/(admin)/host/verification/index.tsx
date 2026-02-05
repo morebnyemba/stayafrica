31d7a98572fee048050cd1f3a930872ce547b922
Moreblessing Nyemba <morebnyemba@gmail.com>
2026-02-05 17:53:26 +0200

    Add HostVerificationScreenContainer component to handle host verification

---
+++ web/src/app/(admin)/host/verification/index.tsx
@@ -0,0 +1,9 @@
+import { VerificationWizard } from '../../../../../mobile/src/components/verification/VerificationWizard';
+
+import { HostVerificationScreen } from './HostVerificationScreen';
+
+export const HostVerificationScreenContainer = () => {
+  return (
+    <HostVerificationScreen />
+  );
+};
