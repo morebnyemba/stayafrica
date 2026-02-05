79779195aa40493e3f1407ab8192cef7e08bbb46
Moreblessing Nyemba <morebnyemba@gmail.com>
2026-02-05 08:44:41 +0200

    refactor: improve readability of inputRefs assignment in VerifyEmailPage component

--- web/src/app/(auth)/verify-email/page.tsx
+++ web/src/app/(auth)/verify-email/page.tsx
@@ -167,7 +167,7 @@ export default function VerifyEmailPage() {
               {code.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => { inputRefs.current[index] = el; }}
                   type="text"
                   inputMode="numeric"
                   maxLength={2}
