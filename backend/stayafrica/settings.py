1e7ea28fd1b1598505871baf95f7767951a6b806
Moreblessing Nyemba <morebnyemba@gmail.com>
2026-03-03 02:48:45 +0200

    feat: configure Celery to use DatabaseScheduler for task scheduling

--- backend/stayafrica/settings.py
+++ backend/stayafrica/settings.py
@@ -357,6 +357,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60  # 30 minutes
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 from celery.schedules import crontab
 
